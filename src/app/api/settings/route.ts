ddbe856b76233080c634c527ff57a5177f906b22
jmattftan <jamesmatthewftan@gmail.com>
2024-10-10 13:45:37 +0800

    Enhanced Security.

--- src/app/api/settings/route.ts
+++ src/app/api/settings/route.ts
@@ -3,8 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   console.log(req.url);
   try {
-    const response = await fetch(process.env.SETTINGS!, {
+    const response = await fetch(process.env.SETTINGS as string, {
       method: "GET",
+      headers: {
+        "Api-Key": process.env.DATA_API_KEY as string,
+      },
     });
     const data = await response.json();
     if (response.ok) {
@@ -23,9 +26,12 @@ export async function PUT(req: NextRequest) {
   try {
     const { username, background_image, background_intensity, theme } =
       await req.json();
-    const response = await fetch(process.env.SETTINGS!, {
+    const response = await fetch(process.env.SETTINGS as string, {
       method: "PUT",
-      headers: { "Content-type": "application/json" },
+      headers: {
+        "Content-type": "application/json",
+        "Api-Key": process.env.DATA_API_KEY as string,
+      },
       body: JSON.stringify({
         username,
         background_image,
--- src/app/api/url_shortcuts/route.ts
+++ src/app/api/url_shortcuts/route.ts
@@ -3,8 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   console.log(req.url);
   try {
-    const response = await fetch(process.env.URL_SHORTCUTS!, {
+    const response = await fetch(process.env.URL_SHORTCUTS as string, {
       method: "GET",
+      headers: {
+        "Api-Key": process.env.DATA_API_KEY as string,
+      },
     });
     const data = await response.json();
     if (response.ok) {
@@ -22,9 +25,12 @@ export async function GET(req: NextRequest) {
 export async function POST(req: NextRequest) {
   try {
     const { name, url } = await req.json();
-    const response = await fetch(process.env.URL_SHORTCUTS!, {
+    const response = await fetch(process.env.URL_SHORTCUTS as string, {
       method: "POST",
-      headers: { "Content-type": "application/json" },
+      headers: {
+        "Content-type": "application/json",
+        "Api-Key": process.env.DATA_API_KEY as string,
+      },
       body: JSON.stringify({
         name: name,
         url: url,
@@ -48,14 +54,20 @@ export async function POST(req: NextRequest) {
 export async function PUT(req: NextRequest) {
   try {
     const { id, name, url } = await req.json();
-    const response = await fetch(`${process.env.URL_SHORTCUTS!}/${id}`, {
-      method: "PUT",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({
-        name: name,
-        url: url,
-      }),
-    });
+    const response = await fetch(
+      `${process.env.URL_SHORTCUTS as string}/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+          "Api-Key": process.env.DATA_API_KEY as string,
+        },
+        body: JSON.stringify({
+          name: name,
+          url: url,
+        }),
+      }
+    );
     if (response.ok) {
       return NextResponse.json(
         { message: "URL Updated" },
@@ -74,13 +86,20 @@ export async function PUT(req: NextRequest) {
 export async function DELETE(req: NextRequest) {
   try {
     const { id, name, url } = await req.json();
-    const response = await fetch(`${process.env.URL_SHORTCUTS!}/${id}`, {
-      method: "DELETE",
-      body: JSON.stringify({
-        name: name,
-        url: url,
-      }),
-    });
+    const response = await fetch(
+      `${process.env.URL_SHORTCUTS as string}/${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-type": "application/json",
+          "Api-Key": process.env.DATA_API_KEY as string,
+        },
+        body: JSON.stringify({
+          name: name,
+          url: url,
+        }),
+      }
+    );
 
     if (response.ok) {
       return NextResponse.json(
